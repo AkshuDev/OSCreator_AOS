356ae29e34d83340e44da41020b15b5f7f289e6d
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-03-09 23:49:19 +0530

    Update VM.py

--- AOS/OSCreator/VM/VM.py
+++ AOS/OSCreator/VM/VM.py
@@ -2,6 +2,9 @@ import os
 import subprocess
 import importlib.util
 import sys
+import pyvbox
+
+from OSCreator.Modules.VirtualBoxSDK.sdk.installer import vboxapi
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -17,7 +20,9 @@ class Dependency:
             importlib.util.find_spec('lzma')
             importlib.util.find_spec('pyvmomi')
             importlib.util.find_spec('configparser')
-            importlib.util.find_spec('cython')
+            importlib.util.find_spec('pyvbox')
+            importlib.util.find_spec('vboxapi')
+            importlib.util.find_spec('virtualbox')
             return True
         except Exception:
             return False
@@ -32,7 +37,9 @@ class Dependency:
             subprocess.run(['pip', 'install', 'lzma'])
             subprocess.run(['pip', 'install', 'pyvmomi'])
             subprocess.run(['pip', 'install', 'configparser'])
-            subprocess.run(['pip', 'install', 'cython'])
+            subprocess.run(['pip', 'install', 'pyvbox'])
+            subprocess.run(['pip', 'install', 'vboxapi'])
+            subprocess.run(['pip', 'install', 'virtualbox'])
         else:
             exit(1)
 
@@ -40,14 +47,9 @@ class Dependency:
     def createCONFIG():
         import configparser
         config = configparser.ConfigParser()
-        config['Modules'] = {'tarfile': 'installed', 'lzma': 'installed', 'pyvmomi': 'installed', 'configparser':'installed', 'cython': 'installed', 'all': 'installed'}
+        config['Modules'] = {'tarfile': 'installed', 'lzma': 'installed', 'pyvmomi': 'installed', 'configparser':'installed', 'pyvbox': 'installed', 'vboxapi': 'installed', 'virtualbox': 'installed', 'all': 'installed'}
         with open(libw.CONFIG, 'w') as cf:
             config.write(cf)
-
-class VM:
-    def __init__(self):
-        self.host = ''
-
 
 if os.path.exists(libw.CONFIG) and Dependency().checkModules():
     pass
@@ -59,3 +61,15 @@ else:
         Dependency().installModules()
         Dependency().createCONFIG()
         exit(0)
+
+import virtualbox as vb
+from Modules.VirtualBoxSDK.sdk.installer.vboxapi import *
+
+class VM:
+    def __init__(self):
+        self.vbox = vboxapi.VirtualBoxManager(None, None)
+
+    def start(self, vname):
+        session = self.vbox.openMachineSession(vname, False)
+
+VM()

6b09d2a98e98b0282f8a93aecfbdfdbcfcba5e15
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-03-18 21:58:48 +0530

    Update VM.py

--- AOS/OSCreator/VM/VM.py
+++ AOS/OSCreator/VM/VM.py
@@ -1,10 +1,11 @@
 import os
+from shutil import ExecError
 import subprocess
 import importlib.util
 import sys
 import pyvbox
 
-from OSCreator.Modules.VirtualBoxSDK.sdk.installer import vboxapi
+from vboxapi import VirtualBoxManager
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -62,14 +63,146 @@ else:
         Dependency().createCONFIG()
         exit(0)
 
+class INI():
+    def __init__(self) -> None:
+        self.file = libw.CONFIG
+
+    @staticmethod
+    def reset():
+        os.remove(libw.CONFIG)
+        raise Exception("Exiting... Please create Restart the script.")
+
+    def settings(self):
+        import configparser
+        config = configparser.ConfigParser()
+        settings = {
+            'port': '80',
+            'VM_port': '$AOS(vm_specified)',
+            'HDisk_port': '81',
+        }
+        config['settings']
+
+    @staticmethod
+    def splitter(cmd: str, flag:str=""):
+        if flag == "PC_Code(FILE)":
+            cmd = cmd.replace('---@---$AOS(', '')
+            cmd = cmd.replace(')---@---', '')
+            cmdL = cmd.split('-')
+            if not cmdL[0].lower() == "pheonix":
+                raise Exception("Invalid Pheonix Code")
+            cmd = str(cmdL[1])
+            file = ""
+            ext = ""
+            for char in cmd:
+                if char.isupper():
+                    file += char
+                if char.islower():
+                    break
+            cmd = cmd.replace(file, '')
+            for char_ in cmd:
+                if char_.islower():
+                    ext += char_
+                if char_.isupper():
+                    break
+            cmd = cmd.replace(ext, '')
+            fullfile = file+'.'+ext
+            func = ''
+            for char__ in cmd:
+                if char__.isupper():
+                    func += char__
+                if char__.isdigit():
+                    break
+            cmd = cmd.replace(func, '')
+            line_no = 0
+            for num in cmd:
+                if num.isdigit() or num == "0":
+                    line_no += int(num)
+                if num.isalpha() and num != '0':
+                    break
+
+            cmd = cmd.replace(str(line_no), '')
+            cmd = cmd.replace('_', '')
+            var = ''
+            self_ = ''
+            callup = ''
+            for self__ in cmd:
+                if self__.islower():
+                    self_ += self__
+                if self__.isupper():
+                    break
+            cmd = cmd.replace(self_, '')
+            for var_ in cmd:
+                if var_.isupper():
+                    var += var_
+                if var_.islower():
+                    break
+            cmd = cmd.replace(var, '')
+            for callup_ in cmd:
+                if callup_.islower():
+                    callup += callup_
+                if callup_.isupper():
+                    break
+            cmd = cmd.replace(callup, '')
+            cmdL = {
+                'file': fullfile,
+                'function': func,
+                'line_no': line_no,
+                'self': self_,
+                'variable': var,
+                'call': callup
+            }
+            return cmdL
+
+    def dict_(self, cmd: list, flag:str=""):
+        from LIB import Dictionary as Dict
+        from LIB import logger as log
+        if flag == 'find':
+            for i, cmd_ in cmd:
+                if '---@---$AOS(' in cmd_ and ')---@---' in cmd_:
+                    tokens = self.splitter(cmd_, 'PC_Code(FILE)')
+                    return tokens
+                else:
+                    if Dict().cmds() in cmd_:
+                        if Dict().checkFORMAT(cmd_):return True
+                        else:return False
+        else:
+            raise Exception(f'{log("", "Wrong Command given", "dict_", "INI", "VM.py", "OSCreator")}')
+
+    def parse(self, cmd: list, flag:str=""):
+        tokens = self.dict_(list('find'),flag)
+        if tokens:
+            pass
+        else:
+            raise Exception('Sorry direct [parse] calls are system hazard!')
+
+    def run(self, cmd: str):
+        cmd = cmd.strip(" ")
+        if self.dict_(list(cmd)):
+            self.parse(list(cmd), "---@---$AOS(pheonix-VMpyINIT6_selfCMD:parse)---@---")
+
 import virtualbox as vb
-from Modules.VirtualBoxSDK.sdk.installer.vboxapi import *
+from virtualbox.library import *
+import vboxapi as vboxapi
 
 class VM:
     def __init__(self):
-        self.vbox = vboxapi.VirtualBoxManager(None, None)
+        self.vboxMGR = VirtualBoxManager(None,  None)
+        self.vbox = vb.VirtualBox()
 
-    def start(self, vname):
-        session = self.vbox.openMachineSession(vname, False)
+    def start(self, vm_name, path=""):
+        vmS = vb.Session()
+        vmM = self.vbox.create_machine(vm_name, "MyVM OS", [], 'Other', '')
+        #configue
+        vmM.memory_size = 100
+        vmM.cpu_count = 1
+        #V Hard disk
+        #vdi_format =
+        vdi_location = path
+        vdi_access_mode = "ReadWrite"
+        vdi_device_type = DeviceType("HardDisk")
+        #vdi = self.vbox.create_medium(MediumVariant("VDI"), vdi_path, [])
+        #vmM.attach_device("SATA", INI().run('pheonix -cmd get --p --hdisk'), vdi_device_type, )
 
-VM()
+
+        progress = vmM.launch_vm_process(vmS, "gui", [])
+        progress.wait_for_completion(10)

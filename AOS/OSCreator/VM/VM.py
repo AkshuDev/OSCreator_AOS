790b6ff6f93c1a62677e74e344e1ce17c50a487d
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-03-19 00:27:18 +0530

    Update VM.py\

--- AOS/OSCreator/VM/VM.py
+++ AOS/OSCreator/VM/VM.py
@@ -6,62 +6,11 @@ import sys
 import pyvbox
 
 from vboxapi import VirtualBoxManager
+import LIB
+
+libw = LIB.Win
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from LIB import Win
-
-libw = Win
-
-class Dependency:
-    @staticmethod
-    def checkModules():
-        try:
-            importlib.util.find_spec('tarfile')
-            importlib.util.find_spec('lzma')
-            importlib.util.find_spec('pyvmomi')
-            importlib.util.find_spec('configparser')
-            importlib.util.find_spec('pyvbox')
-            importlib.util.find_spec('vboxapi')
-            importlib.util.find_spec('virtualbox')
-            return True
-        except Exception:
-            return False
-
-    @staticmethod
-    def installModules():
-        inp = input('Install Modules [5] (y/n): ').lower()
-        if inp == "n":
-            raise Exception("Stopping.. All modules required to use OSCreator")
-        elif inp == 'y':
-            subprocess.run(['pip', 'install', 'tarfile'])
-            subprocess.run(['pip', 'install', 'lzma'])
-            subprocess.run(['pip', 'install', 'pyvmomi'])
-            subprocess.run(['pip', 'install', 'configparser'])
-            subprocess.run(['pip', 'install', 'pyvbox'])
-            subprocess.run(['pip', 'install', 'vboxapi'])
-            subprocess.run(['pip', 'install', 'virtualbox'])
-        else:
-            exit(1)
-
-    @staticmethod
-    def createCONFIG():
-        import configparser
-        config = configparser.ConfigParser()
-        config['Modules'] = {'tarfile': 'installed', 'lzma': 'installed', 'pyvmomi': 'installed', 'configparser':'installed', 'pyvbox': 'installed', 'vboxapi': 'installed', 'virtualbox': 'installed', 'all': 'installed'}
-        with open(libw.CONFIG, 'w') as cf:
-            config.write(cf)
-
-if os.path.exists(libw.CONFIG) and Dependency().checkModules():
-    pass
-else:
-    if Dependency().checkModules():
-        Dependency().createCONFIG()
-        exit(0)
-    else:
-        Dependency().installModules()
-        Dependency().createCONFIG()
-        exit(0)
 
 class INI():
     def __init__(self) -> None:

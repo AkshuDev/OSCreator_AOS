35f019b963baf45fe0dadb906ade4e6394ee858a
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-12-28 14:00:38 +0530

    Delete AOS/AOS-0.1 directory

--- AOS/AOS-0.1/make.bat
+++
@@ -1,4 +0,0 @@
-gcc -c -o "C:\Dev-Cpp\OS Creator\templates\C\kernel.o" "C:\Dev-Cpp\OS Creator\templates\C\kernel.c" 
-nasm -f win32 -o "C:\Dev-Cpp\OS Creator\templates\C\asmkernel.o" "C:\Dev-Cpp\OS Creator\templates\C\kernel.asm"
-ld -o "C:\Dev-Cpp\OS Creator\templates\C\kernel.bin" "C:\Dev-Cpp\OS Creator\templates\C\kernel.o" "C:\Dev-Cpp\OS Creator\templates\C\asmkernel.o" 
-
